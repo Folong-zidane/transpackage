0967fdca9a50a027feba7fcb4c2c37fab2fe3fec
Audain672 <melileffy@gmail.com>
2025-05-07 09:47:45 +0100

    Ajout du service API de relay point

---
+++ api_service/src/test/java/api/point_interet/api_service/ApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package api.point_interet.api_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
